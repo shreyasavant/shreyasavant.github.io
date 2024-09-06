1fef93015ce68c8924eef3f71028753e97669b41
shreyasavant <shreyasavant10@gmail.com>
2024-09-06 03:00:11 -0400

    Changed font to adapt for OS in CSS

--- script.js
+++ script.js
@@ -22,16 +22,16 @@ for (var i=0; themeDots.length > i; i++){
 
 function setTheme(mode){
     if(mode == 'light'){
-        document.getElementById('theme-style').href='default.css'
+        document.getElementById('theme-style').href='stylesheets/default.css'
     }
     if(mode == 'dark'){
-        document.getElementById('theme-style').href='dark.css'
+        document.getElementById('theme-style').href='stylesheets/dark.css'
     }
     if(mode == 'green'){
-        document.getElementById('theme-style').href='green.css'
+        document.getElementById('theme-style').href='stylesheets/green.css'
     }
     if(mode == 'purple'){
-        document.getElementById('theme-style').href='purple.css'
+        document.getElementById('theme-style').href='stylesheets/purple.css'
     }
     localStorage.setItem('theme', mode)
 }
